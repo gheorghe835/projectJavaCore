8ecd670075e33ed3c435cf5a0d6016ab6706e656
gheorghe835 <gheorghe835@gmail.com>
2024-12-29 11:22:57 +0200

    adaugarea conditiei tascului in clasa Main

--- VolumeCylinder/src/Main.java
+++ VolumeCylinder/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Program to calculate cylinder volume\n" + //
+                        "________________________________________\n" + //
+                        "Exercise\n" + //
+                        "A Java program is to be created that will calculate the volume of a cylinder whose radius is 10 and height is 50.\n" + //
+                        "The formula needed to calculate the volume of the cylinder is:\n" + //
+                        "V = r2 * Pi * H\n" + //
+                        "r - the radius of the base of the cylinder\n" + //
+                        "H - height\n" + //
+                        "Pi - constant Pi, 3.14\n" + //
+                        "\n" + //
+                        "The obtained value must be written to the output.\n" + //
+                        "\n" + //
+                        "Translated with www.DeepL.com/Translator (free version)");
     }
 }
